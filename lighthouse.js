21a7503833bd28043d0b4909e298a7b774d2aa05
chrisgates03 <christopheregates@gmail.com>
2026-05-08 20:03:59 -0400

    Update lighthouse.js

--- lighthouse.js
+++ lighthouse.js
@@ -1,1 +1,32 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiY2hyaXNnYXRlczAzIiwiYSI6ImNtbm94N2hnYjIxaGEycXB0MjQ2ajV3c2UifQ.KTGx-0S7nnLj5ZyknIUkVw';
+      const map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/chrisgates03/cmnoytwpa002g01qo9x6za0su',
+        center: [-70.68958, 42.21140], 
+        zoom: 7
+      });
+map.on('load', () => {
+const layers = [
+  'Lighthouse'
+];
+const colors = [
+  '#e7de32'
+];
 
+const legend = document.getElementById('legend');
+
+layers.forEach((layer, i) => {
+  const color = colors[i];
+  const item = document.createElement('div');
+  const key = document.createElement('span');
+  key.className = 'legend-key';
+  key.style.backgroundColor = color;
+
+  const value = document.createElement('span');
+  value.innerHTML = `${layer}`;
+  item.appendChild(key);
+  item.appendChild(value);
+  legend.appendChild(item);
+});
+
+});
